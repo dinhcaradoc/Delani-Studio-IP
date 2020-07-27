c67bffbaa54b6579a22a1037e16d68fcdf39838c
dinhcaradoc <kwambugualex@gmail.com>
2020-07-27 15:26:52 +0300

    update index.html with missing components

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,12 @@ $(document).ready(function(){
     $("#design-icon-hidden").toggle();
     $("#design-icon-showing").toggle();
   })
-})
 
-$(document).ready(function(){
   $("#development-icon").click(function(){
     $("#development-icon-hidden").toggle();
     $("#development-icon-showing").toggle();
   })
-})
 
-$(document).ready(function(){
   $("#product-mgt-icon").click(function(){
     $("#product-mgt-icon-hidden").toggle();
     $("#product-mgt-icon-showing").toggle();
